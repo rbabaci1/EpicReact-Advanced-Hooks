f703265d3330d7047fafaabc387ee7a3274507cf
rbabaci1 <4rabah@gmail.com>
2023-11-24 15:14:23 -0800

    update the state from a variable to an object

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -3,14 +3,17 @@
 
 import * as React from 'react'
 
-function countReducer(state, newState) {
-  return newState
+function countReducer(state, action) {
+  return {...state, ...action}
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [count, setCount] = React.useReducer(countReducer, initialCount)
+  const [state, setState] = React.useReducer(countReducer, {
+    count: initialCount,
+  })
 
-  const increment = () => setCount(count + step)
+  const {count} = state
+  const increment = () => setState({count: count + step})
 
   return <button onClick={increment}>{count}</button>
 }
